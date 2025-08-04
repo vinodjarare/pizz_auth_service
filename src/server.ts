b8a323a1ca0064fcbd910229875cfa87fbab62f6
Vinod Jarare <vinod.j@cloudxperte.com>
2025-08-04 20:55:51 +0530

    chore: Initialize project with package.json, package-lock.json, tsconfig.json, and server.ts

---
+++ src/server.ts
@@ -0,0 +1,6 @@
+function myName(name:string): string {
+  console.log(`Hello, ${name}!`);
+  return `Hello, ${name}!`;
+}
+
+myName("server"); // This will cause a type error since 10 is not a string
