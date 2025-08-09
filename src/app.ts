5282611e55db0b319f4ef7e798011babc555b408
Vinod Jarare <vinod.j@cloudxperte.com>
2025-08-09 14:04:18 +0530

    feat: add global error handler and update dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome to auth service");
 });
+
+app.use(globalErrorHandler);
 
 export default app;
